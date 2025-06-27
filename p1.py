15f665feac63900b873728f0927782f3a2c81193
AndrewZakri <zakria@sunypoly.edu>
2025-06-27 15:00:36 -0400

    Create p1.py

---
+++ p1.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import folium
+from folium import plugins
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from streamlit_folium import st_folium
+
+# Load data - filtered for BOS
+df = pd.read_csv("BOS_Jan_25.csv")
+bos_routes = df[df['Origin'] == 'BOS'][['Origin', 'Dest', 'Dest_lat', 'Dest_long']].drop_duplicates()
+
+# Create base map
+m = folium.Map(location=[42.3656, -71.0096], zoom_start=4)
+
+# Add lines for each direct route
+for _, row in bos_routes.iterrows():
+    folium.PolyLine(
+        locations=[
+            [42.3656, -71.0096],
+            [row['Dest_lat'], row['Dest_long']]
+        ],
+        color="blue",
+        weight=2,
+        opacity=0.6
+    ).add_to(m)
+
+# Creat map for direct destinations from BOS
+st_folium(m, width=725, height=500)
